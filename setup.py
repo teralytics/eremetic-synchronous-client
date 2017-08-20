510a6ec1a77ada648d344b7f0f086089814d7bef
Stefano Baghino <stefano@baghino.me>
2017-08-20 13:53:53 +0200

    [bootstrap] This release is driving me crazy

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from distutils.core import setup
 setup(
   name = 'eremetic-synchronous-client',
   py_modules=['eremetic_synchronous_client'],
-  version = '0.28.0',
+  version = '0.28.1',
   description = 'A synchronous client for Eremetic',
   author = 'Stefano Baghino',
   author_email = 'stefano@baghino.me',
   url = 'https://github.com/stefanobaghino/eremetic-synchronous-client',
-  download_url = 'https://github.com/stefanobaghino/eremetic-synchronous-client/archive/0.28.0.tar.gz',
-  keywords = 'eremetic mesos',
+  keywords = ['eremetic', 'mesos'],
   install_requires=['requests>=2.8.13'],
   classifiers = [],
 )
